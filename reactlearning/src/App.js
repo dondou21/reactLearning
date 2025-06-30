6cd8c4558e05079a9c81a6ec9e24068e892a99b1
Gym Uruhimbi <gymuruhimbi@uruhimbis-iMac.local>
2025-06-30 17:11:17 +0200

    Nothing ti say

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Header, Footer } from "./Mycomponent.js";
+import MainContent  from './Mycomponent.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Header />
+    <MainContent />
+    <Footer />
+  </div>
   );
 }
 
 export default App;
+
+
+function Greet(props) {
+  return <h2> Hi, {props.username}!</h2>;
+}
+
+<Greet username="Joly" />
---
+++ reactlearning/src/Mycomponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+    return <h1> Welcome , this is the header</h1>;
+};
+
+export const Footer = () => {
+    return <footer>© 2023 My Website</footer>;
+};
+
+export default function MainContent() {
+    return <div>
+        This is the main content of our website
+    </div>;
+};
