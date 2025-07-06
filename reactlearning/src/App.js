1474f2aff4b86f22f75e99220f89973cbe911414
dondou21 <dondouabiyi@gmail.com>
2025-07-06 16:43:33 +0200

    Nothing to commit

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createContext, useContext } from 'react';
 import './App.css';
 import { Header, Footer } from "./Mycomponent.js";
 import MainContent  from './Mycomponent.js';
@@ -93,12 +93,53 @@ const users = [
 //   )
 // }
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <UserProvider>
+//       <WelcomePage />
+//     </UserProvider>
+//   )
+// }
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const increment = () => setCount(count +1);
+//   const decrement = () => setCount(count -1);
+
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={increment}>increment</button>
+//       <button onClick={decrement}>decrement</button>
+//     </div>
+//   )
+
+// }
+
+const ThemeContext = createContext();
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <UserProvider>
-      <WelcomePage />
-    </UserProvider>
-  )
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <h1>Current Theme: {theme}</h1>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <ThemeDisplay />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+function ThemeDisplay() {
+  const theme = useContext(ThemeContext);
+  
+  return <h2>Them from Context: {ThemeContext}</h2>;
 }
 
 export default App;
